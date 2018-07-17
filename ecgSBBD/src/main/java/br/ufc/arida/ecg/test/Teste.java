16f3b2f9de48d233e02119c3ed3080873691e236
tiago vinuto <you@example.com>
2018-07-17 20:16:20 -0300

    alteraçõe

---
+++ ecgSBBD/src/main/java/br/ufc/arida/ecg/test/Teste.java
@@ -0,0 +1,28 @@
+package br.ufc.arida.ecg.test;
+
+public class Teste {
+	public static void main(String[] args) {
+		
+	//String x = 	new String[]{"-u", mc.getDatabaseUser(), "-pass", mc.getDatabasePassword(), "-d", DbConnection.getDriverClass(mc.getDatabaseDriver()), "-j", mc.getDatabaseUrl(), "r2rml.ttl"}
+	
+		
+	}
+
+/*	public void publishR2rmlFired(ActionEvent event) throws IOException, Exception {
+        File f = new File("r2rml.ttl");
+        FileUtils.writeStringToFile(f, r2rml.toString().replaceAll("&lt;", "<").replaceAll("&gt;", ">"));
+
+        server s = new server();
+        try {
+            s.process(new String[]{"-u", mc.getDatabaseUser(), "-pass", mc.getDatabasePassword(), "-d", DbConnection.getDriverClass(mc.getDatabaseDriver()), "-j", mc.getDatabaseUrl(), "r2rml.ttl"});
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "It was not possible to start D2RQ beacuse: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+        }
+
+        URI u = new URI("http://localhost:2020/snorql/");
+        Desktop.getDesktop().browse(u);
+    }*/
+	
+	
+
+}

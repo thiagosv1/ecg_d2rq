021618bfb88e5ae080243921f4e0229d688fc011
salomao santos <salomaosilvasantos@gmail.com>
2018-07-24 13:47:51 -0300

    funcionando

--- ecgSBBD/src/main/java/br/ufc/arida/ecg/test/Teste.java
+++ ecgSBBD/src/main/java/br/ufc/arida/ecg/test/Teste.java
@@ -1,28 +1,23 @@
 package br.ufc.arida.ecg.test;
 
+import d2rq.dump_rdf;
+
 public class Teste {
+	//-o /home/salomao/git/ecg_d2rq/ecgSBBD/src/main/resources/saidalll.nt 
+	//"/home/salomao/git/ecg_d2rq/ecgSBBD/src/main/resources/mapping-iswc.ttl"
 	public static void main(String[] args) {
+       
+		String arguments [] = {"-b", "http://arida.ufc.br/ecg/", "-o", "/home/salomao/git/ecg_d2rq/ecgSBBD/src/main/resources/output.nt", "/home/salomao/git/ecg_d2rq/ecgSBBD/src/main/resources/mapping-iswc.ttl"};
 		
-	//String x = 	new String[]{"-u", mc.getDatabaseUser(), "-pass", mc.getDatabasePassword(), "-d", DbConnection.getDriverClass(mc.getDatabaseDriver()), "-j", mc.getDatabaseUrl(), "r2rml.ttl"}
-	
-		
+		try {
+			dump_rdf dump = new  dump_rdf();
+			
+			dump.process(arguments);
+			
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 	}
 
-/*	public void publishR2rmlFired(ActionEvent event) throws IOException, Exception {
-        File f = new File("r2rml.ttl");
-        FileUtils.writeStringToFile(f, r2rml.toString().replaceAll("&lt;", "<").replaceAll("&gt;", ">"));
-
-        server s = new server();
-        try {
-            s.process(new String[]{"-u", mc.getDatabaseUser(), "-pass", mc.getDatabasePassword(), "-d", DbConnection.getDriverClass(mc.getDatabaseDriver()), "-j", mc.getDatabaseUrl(), "r2rml.ttl"});
-        } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, "It was not possible to start D2RQ beacuse: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
-        }
-
-        URI u = new URI("http://localhost:2020/snorql/");
-        Desktop.getDesktop().browse(u);
-    }*/
-	
-	
 
 }

b1347ecbf978220cab3e411347f16e4484455b22
LAPTOP-0VUT3568\salomao <salomaosilvasantos>
2018-08-09 17:12:26 -0300

    mapeamento d2rq e classe teste usando o mapeamento r2rml feito pelo Douglas

---
+++ aplicacao/ecgSBBD/src/main/java/br/ufc/arida/ecg/test/TesteD2RQ.java
@@ -0,0 +1,29 @@
+package br.ufc.arida.ecg.test;
+
+import java.io.File;
+
+import d2rq.dump_rdf;
+
+public class TesteD2RQ {
+
+	public static void main(String[] args) {
+		
+		try {
+			
+			String url = new File("src/main/resources/").getAbsolutePath(); //precisamos desse caminho para funcionar em diversos S.O.
+	        String arquivo_mapeamento = url +"/mapping/mapeamentoecg_d2rq.ttl";
+			String arquivo_saida = url + "/triples/output.nt";
+			String uri = "http://arida.ufc.br/ecg/";
+	                
+			String argumentos [] = {"-b", uri, "-o", arquivo_saida, arquivo_mapeamento };
+
+			dump_rdf dump = new  dump_rdf();
+			dump.process(argumentos);
+			
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+
+}

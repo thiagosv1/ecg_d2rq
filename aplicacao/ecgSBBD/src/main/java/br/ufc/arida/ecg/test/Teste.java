80a8133e0d2d8bdc92ab7ba0472fd9707552a51a
salomao santos <salomaosilvasantos@gmail.com>
2019-05-30 22:40:38 -0300

    melhorando o código

--- aplicacao/ecgSBBD/src/main/java/br/ufc/arida/ecg/test/Teste.java
+++ aplicacao/ecgSBBD/src/main/java/br/ufc/arida/ecg/test/Teste.java
@@ -3,6 +3,7 @@ package br.ufc.arida.ecg.test;
 import java.io.File;
 
 import d2rq.dump_rdf;
+import d2rq.generate_mapping;
 
 public class Teste {
 
@@ -11,14 +12,25 @@ public class Teste {
 		try {
 			
 			String url = new File("src/main/resources/").getAbsolutePath(); //precisamos desse caminho para funcionar em diversos S.O.
-	        String arquivo_mapeamento = url +"/mapping/mapping-iswc.ttl";
-			String arquivo_saida = url + "/triples/output.nt";
-			String uri = "http://arida.ufc.br/ecg/";
+	        String arquivo_mapeamento = url +"/mapping/iswc.ttl";
+			String arquivo_saida = url + "/triples/direct_ontology_iswc.owl";
+			String uri = "http://arida.ufc.br/iswc/";
 	                
-			String argumentos [] = {"-b", uri, "-o", arquivo_saida, arquivo_mapeamento };
+            String argumentos2 [] = {"-d","org.postgresql.Driver", "-u","postgres", "-p","postgres", "-o", arquivo_mapeamento, "jdbc:postgresql://localhost:5432/iswc?autoReconnect=true" };
+			
+			generate_mapping generate_mapping = new generate_mapping();
+			
+			generate_mapping.process(argumentos2);
+			
 
-			dump_rdf dump = new  dump_rdf();
-			dump.process(argumentos);
+			String argumentos [] = {"-b", uri, "-f", "RDF/XML-ABBREV", "-o", arquivo_saida, arquivo_mapeamento };
+			  
+			 dump_rdf dump = new dump_rdf(); 
+			 
+			 dump.process(argumentos);
+			 
+			
+			
 			
 		} catch (Exception e) {
 			System.out.println(e);
